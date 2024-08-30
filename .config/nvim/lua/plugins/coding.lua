b057a2cf208f29f6a9633404510581c30e82d1a5
John Tobin <johntobin@johntobin.ie>
2024-08-30 23:04:15 +0100

    Remove treesitter pointers, I have already installed it.

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -49,10 +49,6 @@ return {
   -- Asynchronous linting that reports through neovim's standard reporting.
   -- Similar to ALE but doesn't include an LSP client.
   -- "mfussenegger/nvim-lint",
-  -- Treesitter for syntax highlighting.  See
-  -- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/plugins/treesitter.lua
-  -- for necessary config.
-  -- "nvim-treesitter/nvim-treesitter"
   -- Use Treesitter to define custom text objects.
   -- "nvim-treesitter/nvim-treesitter-textobjects",
 }
