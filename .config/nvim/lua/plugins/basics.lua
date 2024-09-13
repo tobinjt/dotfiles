69866266f2dd75dddf620de404ebd0fc83e5b41c
John Tobin <johntobin@johntobin.ie>
2024-09-13 22:03:12 +0100

    Load vim-fetch for file:line support.

--- .config/nvim/lua/plugins/basics.lua
+++ .config/nvim/lua/plugins/basics.lua
@@ -1,6 +1,12 @@
 return {
   {
+    -- Improved status bar.
     dir = vim.fn.expand("~/.vim/bundle/vim-airline"),
     lazy = false,
-  }
+  },
+  {
+    -- Makes filename:line-number[:column] work.
+    dir = vim.fn.expand("~/.vim/bundle/vim-fetch"),
+    lazy = false,
+  },
 }
