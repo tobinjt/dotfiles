108186d6448b8dccfd214c942b0cc8f86c4a492a
John Tobin <johntobin@johntobin.ie>
2024-09-14 22:39:43 +0100

    Configure Lua support.
    Ideally the config would not be split but I haven't figured out how to
    split them correctly.

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -5,7 +5,6 @@ return {
     --   "mason.nvim",
     --   "mason-lspconfig.nvim",
     -- },
-    lazy = false,
     ft = {
       -- keep-sorted start
       "go",
@@ -15,15 +14,32 @@ return {
       "shell",
       -- keep-sorted end
     },
-    config = function(_, _opts)
+    config = function(_, _)
       local lspconfig = require("lspconfig")
       -- keep-sorted start
       lspconfig.bashls.setup{}
       lspconfig.gopls.setup{}
-      lspconfig.lua_ls.setup{}
       lspconfig.pylsp.setup{}
       lspconfig.rust_analyzer.setup{}
       -- keep-sorted end
+
+      lspconfig.lua_ls.setup {
+        settings = {
+          Lua = {
+            diagnostics = {
+              -- Get the language server to recognize the `vim` global.
+              globals = {
+                'require',
+                'vim'
+              },
+            },
+            workspace = {
+              -- Make the server aware of Neovim runtime files.
+              library = vim.api.nvim_get_runtime_file("", true),
+            },
+          },
+        },
+      }
     end
   },
 
---
+++ .config/nvim/lua/plugins/lua.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "folke/lazydev.nvim",
+    ft = "lua",
+  },
+  -- There is additional config in lsp.lua, because I can't figure out how to
+  -- separate the config for neovim/nvim-lspconfig without overwriting one or
+  -- the other config.
+}
