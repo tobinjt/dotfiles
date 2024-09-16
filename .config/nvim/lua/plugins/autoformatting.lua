83142b9f68c001612b066254d8a6ece52c57d293
John Tobin <johntobin@johntobin.ie>
2024-09-16 14:18:53 +0100

    Setup conform.nvim.

---
+++ .config/nvim/lua/plugins/autoformatting.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    'stevearc/conform.nvim',
+    opts = {
+      -- I can configure additional formatters here, but for now the LSP servers
+      -- are covering all the filetypes I've tested.
+      format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_format = "fallback",
+      },
+    },
+  }
+}
