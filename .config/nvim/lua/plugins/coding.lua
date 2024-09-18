046dd27b11b7982e12417ab7773c890ff80f68b6
John Tobin <johntobin@johntobin.ie>
2024-09-18 21:14:37 +0100

    Remove old TODOs and notes.

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -5,14 +5,4 @@ return {
     dir = vim.fn.expand("~/.vim/bundle/vim-signify"),
     lazy = false,
   },
-
-  -- Windows for LSP diagnostics and other things.
-  -- "folke/trouble.nvim",
-  -- Generic formatting support.  Needs configuration for each formatter.
-  -- "stevearc/conform.nvim",
-  -- Asynchronous linting that reports through neovim's standard reporting.
-  -- Similar to ALE but doesn't include an LSP client.
-  -- "mfussenegger/nvim-lint",
-  -- Use Treesitter to define custom text objects.
-  -- "nvim-treesitter/nvim-treesitter-textobjects",
 }
