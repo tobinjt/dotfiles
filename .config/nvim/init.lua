449f928efeb872e67712fe0a1f0acd07d5ae93dd
John Tobin <johntobin@johntobin.ie>
2024-09-18 20:52:12 +0100

    Work around differences between Lua 5.1 and 5.4.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,9 @@
+-- Neovim is permanently on Lua 5.1, but everything else assumes a later Lua
+-- version, and complains that `unpack` is deprecated and I should use
+-- `table.unpack` instead, which doesn't exist in Lua 5.1, so create it.
+---@diagnostic disable-next-line: deprecated
+table.unpack = table.unpack or unpack
+
 -- Copied from https://lazy.folke.io/installation
 -- Bootstrap lazy.nvim.
 require("config.lazy")
