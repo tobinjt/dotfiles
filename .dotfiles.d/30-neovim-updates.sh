49160228da14923396111bfa728d15c571571458
John Tobin <johntobin@johntobin.ie>
2024-09-26 21:00:52 +0100

    Use run-if-exists.

--- .dotfiles.d/30-neovim-updates.sh
+++ .dotfiles.d/30-neovim-updates.sh
@@ -4,10 +4,8 @@ set -e -f -u -o pipefail
 
 # Sync Neovim plugins.
 main() {
-  if type nvim >& /dev/null; then
-    # Lazy is very chatty, reduce noise.
-    nvim --headless -c "Lazy! restore" -c "qa" > /dev/null
-  fi
+  # Lazy is very chatty, reduce noise.
+  run-if-exists nvim --headless -c "Lazy! restore" -c "qa" > /dev/null
 }
 
 main "$@"
