42638f10935cbf3209156a1cb1b12e05437f8478
John Tobin <johntobin@johntobin.ie>
2024-10-04 23:55:32 +0100

    Got DAP working to some extent!

--- .config/nvim/lua/plugins/debugging.lua
+++ .config/nvim/lua/plugins/debugging.lua
@@ -1,11 +1,16 @@
+-- Docs at https://github.com/mfussenegger/nvim-dap-python are useful.
 return {
   {
     "mfussenegger/nvim-dap",
+    -- lua require'dap'.continue()
   },
 
   -- keep-sorted start block=yes
   {
     "jay-babu/mason-nvim-dap.nvim",
+    -- Untested.
+    enabled = false,
+    config = true,
     dependencies = {
       "mfussenegger/nvim-dap",
       "williamboman/mason.nvim",
@@ -13,6 +18,8 @@ return {
   },
   {
     "leoluz/nvim-dap-go",
+    -- Untested.
+    enabled = false,
     config = true,
     dependencies = {
       "mfussenegger/nvim-dap",
@@ -20,7 +27,12 @@ return {
   },
   {
     "mfussenegger/nvim-dap-python",
-    opts = {},
+    config = function()
+      local python = vim.fn.expand("~/.local/share/nvim/mason/packages/debugpy/venv/bin/python")
+      require("dap-python").setup(python)
+    end,
+    -- Consider the mappings at
+    -- https://github.com/mfussenegger/nvim-dap-python?tab=readme-ov-file#mappings
     dependencies = {
       "mfussenegger/nvim-dap",
     },
@@ -35,7 +47,17 @@ return {
   },
   {
     "rcarriga/nvim-dap-ui",
+    -- lua require("dapui").open()
     config = true,
+    keys = {
+      {
+        "<leader>du",
+        function()
+          require("dapui").toggle({})
+        end,
+        desc = "Dap UI"
+      },
+    },
     dependencies = {
       "mfussenegger/nvim-dap",
       "nvim-neotest/nvim-nio",
