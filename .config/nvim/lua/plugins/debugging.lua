d973e53ed66b47dbeee6935acd6419603f3da183
John Tobin <johntobin@johntobin.ie>
2024-10-05 21:48:10 +0100

    Add more keymappings, copied from LazyVim.

--- .config/nvim/lua/plugins/debugging.lua
+++ .config/nvim/lua/plugins/debugging.lua
@@ -22,11 +22,6 @@ return {
     },
   },
   {
-    "mfussenegger/nvim-dap",
-    -- lua require'dap'.continue()
-    lazy = true,
-  },
-  {
     "mrcjkb/rustaceanvim",
     enabled = false,
     config = true,
@@ -36,7 +31,6 @@ return {
   },
   {
     "rcarriga/nvim-dap-ui",
-    -- lua require("dapui").open()
     config = true,
     keys = {
       {
@@ -60,6 +54,106 @@ return {
         -- https://github.com/mfussenegger/nvim-dap-python?tab=readme-ov-file#mappings
         dependencies = {
           "mfussenegger/nvim-dap",
+          lazy = true,
+          -- Copied from LazyVim/lua/lazyvim/plugins/extras/dap/core.lua and
+          -- modified.
+          keys = {
+            {
+              "<leader>dB",
+              function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end,
+              desc = "Breakpoint Condition"
+            },
+
+            {
+              "<leader>db",
+              function() require("dap").toggle_breakpoint() end,
+              desc = "Toggle Breakpoint"
+            },
+
+            {
+              "<leader>dc",
+              function() require("dap").continue() end,
+              desc = "Continue"
+            },
+
+            {
+              "<leader>dC",
+              function() require("dap").run_to_cursor() end,
+              desc = "Run to Cursor"
+            },
+
+            {
+              "<leader>dg",
+              function() require("dap").goto_() end,
+              desc = "Go to Line (No Execute)"
+            },
+
+            {
+              "<leader>di",
+              function() require("dap").step_into() end,
+              desc = "Step Into"
+            },
+
+            {
+              "<leader>dj",
+              function() require("dap").down() end,
+              desc = "Down"
+            },
+
+            {
+              "<leader>dk",
+              function() require("dap").up() end,
+              desc = "Up"
+            },
+
+            {
+              "<leader>dl",
+              function() require("dap").run_last() end,
+              desc = "Run Last"
+            },
+
+            {
+              "<leader>do",
+              function() require("dap").step_out() end,
+              desc = "Step Out"
+            },
+
+            {
+              "<leader>dO",
+              function() require("dap").step_over() end,
+              desc = "Step Over"
+            },
+
+            {
+              "<leader>dp",
+              function() require("dap").pause() end,
+              desc = "Pause"
+            },
+
+            {
+              "<leader>dr",
+              function() require("dap").repl.toggle() end,
+              desc = "Toggle REPL"
+            },
+
+            {
+              "<leader>ds",
+              function() require("dap").session() end,
+              desc = "Session"
+            },
+
+            {
+              "<leader>dt",
+              function() require("dap").terminate() end,
+              desc = "Terminate"
+            },
+
+            {
+              "<leader>dw",
+              function() require("dap.ui.widgets").hover() end,
+              desc = "Widgets"
+            },
+          },
         },
       },
       {
