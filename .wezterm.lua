3455b96f388fe859268d16efd76b9aa292a35e19
John Tobin <johntobin@johntobin.ie>
2024-12-14 17:31:02 +0000

    feat: Add section headers.

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,7 +1,10 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
+-- Colour scheme.
 config.color_scheme = 'Solarized (dark) (terminal.sexy)'
+
+-- Font configuration.
 config.font = wezterm.font(
   "Inconsolata",
   {
@@ -11,6 +14,7 @@ config.font = wezterm.font(
   })
 config.font_size = 15
 
+-- Recognise colon as a word separator.
 -- https://wezfurlong.org/wezterm/config/lua/config/selection_word_boundary.html
 config.selection_word_boundary = " \t\n{}[]()\"'`" .. ":"
 
