9b793fb2b6de177ac71aafd468cfc48b786f8792
John Tobin <johntobin@johntobin.ie>
2024-12-15 15:01:17 +0000

    feat: Update TreeSitter parsers when running update-dotfiles-and-bin

--- .dotfiles.d/30-neovim-updates.sh
+++ .dotfiles.d/30-neovim-updates.sh
@@ -6,6 +6,9 @@ set -e -f -u -o pipefail
 main() {
   # Lazy is very chatty, reduce noise.
   run-if-exists nvim --headless -c "Lazy! restore" -c "qa" > /dev/null
+  run-if-exists nvim --headless -c "TSUpdate" -c "qa" > /dev/null
+  # TSUpdate outputs a message without a newline.
+  echo
 }
 
 main "$@"
