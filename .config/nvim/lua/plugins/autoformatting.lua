b30d017af26d0298019a0c171d8534a810fdf057
John Tobin <johntobin@johntobin.ie>
2025-02-02 22:20:40 +0000

    feat: Run keep-sorted for every file type.

--- .config/nvim/lua/plugins/autoformatting.lua
+++ .config/nvim/lua/plugins/autoformatting.lua
@@ -8,20 +8,39 @@ return {
     -- Requires Neovim 0.10 or later.
     cond = vim.fn.has("nvim-0.10") == 1,
     opts = {
-      -- I can configure additional formatters here, but for now the LSP servers
-      -- are covering all the file types I've tested.
       format_on_save = {
         -- These options will be passed to conform.format()
         timeout_ms = 5000,
         lsp_format = "fallback",
       },
+
+      -- Run keep-sorted for every file type.
+      -- This runs async after write, but it runs so fast that being async
+      -- shouldn't be a problem.
+      format_after_save = {
+        -- These options will be passed to conform.format()
+        formatters = {
+          "keep_sorted",
+        },
+      },
+
       formatters_by_ft = {
         markdown = {
           "mdformat",
           "prettier",
         },
       },
+
       formatters = {
+        keep_sorted = {
+          command = "keep-sorted",
+          -- This is ignored in many cases :(
+          stdin = false,
+          -- Force adding a filename so that keep-sorted works.
+          args = {
+            "$FILENAME",
+          },
+        },
         mdformat = {
           prepend_args = {
             "--wrap",
