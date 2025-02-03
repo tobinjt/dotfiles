48455351b4fae365e47ebda0beff0f79de0a81d8
John Tobin <johntobin@johntobin.ie>
2025-02-03 08:52:21 +0000

    docs: testing shows buffer-local textwidth is used and the autocmd fires after FileType autocmds.

--- .config/nvim/lua/config/colorcolumn.lua
+++ .config/nvim/lua/config/colorcolumn.lua
@@ -1,14 +1,14 @@
 -- Highlight the three columns after textwidth with a different color, but
 -- only if there is text to highlight.
 -- TODO: need to disable this for certain filetypes, e.g. help and golang.
--- TODO: does this fire after filetype is set or before?
--- TODO: does this use global textwidth or buffer-local textwidth?
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufReadPost" }, {
   desc = "Highlight column after textwidth",
   -- Relies on the augroup being created in autocmd.vim.
   group = "johntobin",
   callback = function()
     -- See: :help /\zs; matches after the specified column.
+    -- This uses buffer-local textwidth, and in practice it fires after the
+    -- FileType autocmds, but I can't find documentation to confirm that.
     vim.fn.matchadd('WarningMsg', '^.\\{' .. vim.o.textwidth .. '}\\zs.\\{1,3}')
   end,
 })
