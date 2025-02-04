48916a34d5812b589675612eb3a9495d2fda1beb
John Tobin <johntobin@johntobin.ie>
2025-02-04 11:16:19 +0000

    feat: support disabling autoformatting.

--- .config/nvim/lua/plugins/autoformatting.lua
+++ .config/nvim/lua/plugins/autoformatting.lua
@@ -8,11 +8,21 @@ return {
     -- Requires Neovim 0.10 or later.
     cond = vim.fn.has("nvim-0.10") == 1,
     opts = {
-      format_on_save = {
-        -- These options will be passed to conform.format()
-        timeout_ms = 5000,
-        lsp_format = "fallback",
-      },
+      -- Support for enable/disable autoformat-on-save.
+      -- Modified from
+      -- https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#command-to-toggle-format-on-save
+      format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable.
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return {
+          -- These options will be passed to conform.format().
+          -- Long timeout because large Python files can be slow to format.
+          timeout_ms = 5000,
+          lsp_format = "fallback",
+        }
+      end,
 
       -- Run keep-sorted for every file type.
       -- This runs async after write, but it runs so fast that being async
@@ -49,5 +59,36 @@ return {
         },
       },
     },
+
+    config = function(_, opts)
+      require("conform").setup(opts)
+
+      -- Add commands to enable/disable autoformat-on-save.
+      -- Modified from
+      -- https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#command-to-toggle-format-on-save
+      vim.g.disable_autoformat = false
+      vim.b.disable_autoformat = false
+      vim.api.nvim_create_user_command("FormatDisable",
+        function(args)
+          if args.bang then
+            -- FormatDisable! will disable formatting globally
+            vim.g.disable_autoformat = true
+          else
+            vim.b.disable_autoformat = true
+          end
+        end,
+        {
+          desc = "Disable autoformat-on-save",
+          bang = true,
+        })
+      vim.api.nvim_create_user_command("FormatEnable",
+        function()
+          vim.b.disable_autoformat = false
+          vim.g.disable_autoformat = false
+        end,
+        {
+          desc = "Re-enable autoformat-on-save",
+        })
+    end,
   }
 }
