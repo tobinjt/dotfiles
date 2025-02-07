436bd6ecd33c61016a6e1f9b8cda074c4f2bf054
John Tobin <johntobin@johntobin.ie>
2025-02-07 22:56:29 +0000

    feat: update highlighting whenever the textwidth option is set.

--- .config/nvim/lua/config/colorcolumn.lua
+++ .config/nvim/lua/config/colorcolumn.lua
@@ -1,22 +1,46 @@
 -- Highlight the three columns after textwidth with a different color, but
 -- only if there is text to highlight.
+local autocmd_callback = function()
+  -- Need a non-zero textwidth.
+  if vim.o.textwidth == 0 then
+    return
+  end
+  -- Some filetypes have really long lines and are too noisy.
+  local filetypes_to_ignore = {
+    -- keep-sorted start
+    'go',
+    'help',
+    -- keep-sorted end
+  }
+  if vim.tbl_contains(filetypes_to_ignore, vim.o.filetype) then
+    return
+  end
+
+  -- Clean up old matches.
+  if vim.b.colorcolum_matchadd_id then
+    vim.fn.matchdelete(vim.b.colorcolum_matchadd_id)
+  end
+
+  -- Create the new match.
+  -- See: :help /\zs; matches after the specified column.
+  -- TODO: experiment with :help /\%v to match on virtual columns rather than
+  -- characters.  The current regex won't work well with tabs because each tab
+  -- is a single character but occupies multiple screen columns.
+  local regex = '^.\\{' .. vim.o.textwidth .. '}\\zs.\\{1,3}'
+  vim.b.colorcolum_matchadd_id = vim.fn.matchadd('WarningMsg', regex)
+end
+
+vim.api.nvim_create_autocmd({ "OptionSet" }, {
+  desc = "Highlight column after textwidth",
+  -- Relies on the augroup being created in autocmd.vim.
+  group = "johntobin",
+  pattern = "textwidth",
+  callback = autocmd_callback,
+})
+
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufReadPost" }, {
   desc = "Highlight column after textwidth",
   -- Relies on the augroup being created in autocmd.vim.
   group = "johntobin",
-  callback = function()
-    local filetypes_to_ignore = {
-      -- keep-sorted start
-      'go',
-      'help',
-      -- keep-sorted end
-    }
-    if vim.tbl_contains(filetypes_to_ignore, vim.o.filetype) then
-      return
-    end
-    -- See: :help /\zs; matches after the specified column.
-    -- This uses buffer-local textwidth, and in practice it fires after the
-    -- FileType autocmds, but I can't find documentation to confirm that.
-    vim.fn.matchadd('WarningMsg', '^.\\{' .. vim.o.textwidth .. '}\\zs.\\{1,3}')
-  end,
+  callback = autocmd_callback,
 })
