c10a55dfd710e017edc715f9ecc02bfeaa8b61af
John Tobin <johntobin@johntobin.ie>
2025-04-02 23:17:10 +0100

    feat: LSP autocompletion.  Untested.

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -55,6 +55,24 @@ return {
           },
         }
       )
+
+      -- https://gpanders.com/blog/whats-new-in-neovim-0-11/#builtin-auto-completion
+      -- LSP autocompletion.
+      vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function(ev)
+          local client = vim.lsp.get_client_by_id(ev.data.client_id)
+          if client == nil then
+            return
+          end
+          if client:supports_method('textDocument/completion') then
+            vim.lsp.completion.enable(
+              true,
+              client.id,
+              ev.buf,
+              { autotrigger = true })
+          end
+        end,
+      })
     end,
 
     opts = {
