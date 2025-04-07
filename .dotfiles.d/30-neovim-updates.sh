503d89f1ce2c25c574fa133db78616a649bcfe17
John Tobin <johntobin@johntobin.ie>
2025-04-08 00:05:14 +0100

    feat: Clean out treesitter .so files, otherwise they are never updated.

--- .dotfiles.d/30-neovim-updates.sh
+++ .dotfiles.d/30-neovim-updates.sh
@@ -4,6 +4,13 @@ set -e -f -u -o pipefail
 
 # Sync Neovim plugins.
 main() {
+  # Clean out treesitter .so files, otherwise they are never updated.
+  # TODO: figure out how to compile the deleted libraries, otherwise it happens
+  # the next time I run nvim interactively.
+  local treesitter_dir="${HOME}/.local/share/nvim/lazy/nvim-treesitter/parser"
+  if [[ -d "${treesitter_dir}" ]]; then
+    find "${treesitter_dir}" -type f -delete
+  fi
   # Lazy is very chatty, reduce noise.
   run-if-exists nvim --headless -c "Lazy! restore" -c "qa" > /dev/null
   run-if-exists nvim --headless -c "TSUpdate" -c "qa" > /dev/null
