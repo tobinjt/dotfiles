8a886a1f2643b89ae9f9c798097088fc0c2a5c6e
John Tobin <johntobin@johntobin.ie>
2025-06-11 17:14:52 +0100

    feat: install MeanderingProgrammer/render-markdown.nvim for in-editor markdown rendering

---
+++ .config/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+    },
+    ft = {
+      "markdown",
+    },
+  },
+}
