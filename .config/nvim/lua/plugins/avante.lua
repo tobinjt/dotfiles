818984cc42fe1738fb323e284811afdf7abdfc20
John Tobin <johntobin@johntobin.ie>
2025-06-12 19:30:22 +0100

    feat: install Avante with Gemini as provider.

---
+++ .config/nvim/lua/plugins/avante.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    version = false, -- Never set this value to "*"! Never!
+    cond = vim.fn.filereadable(vim.fn.expand("~/.config/gemini/api_key")) == 1,
+    opts = {
+      provider = "gemini",
+      providers = {
+        gemini = {
+          model = "gemini-2.5-flash-preview-05-20",
+        },
+      },
+    },
+    build = "make",
+    dependencies = {
+      -- Required.
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      --- Optional,
+      "folke/snacks.nvim", -- for input provider snacks
+      "hrsh7th/nvim-cmp",  -- autocompletion for avante commands and mentions
+      {
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = {
+            "Avante",
+            "markdown",
+          },
+        },
+      },
+    },
+  }
+}
