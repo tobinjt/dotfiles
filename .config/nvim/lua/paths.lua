211cca13bc28545d424cc61cdf0d25686e49a268
John Tobin <johntobin@johntobin.ie>
2025-06-24 09:00:08 +0100

    feat: support checking if a directory exists.

--- .config/nvim/lua/paths.lua
+++ .config/nvim/lua/paths.lua
@@ -6,7 +6,7 @@ local M = {
 -- Expands a given path and checks whether it exists.
 function M.exists(path)
   local expanded = vim.fn.expand(path)
-  return vim.fn.filereadable(expanded) == 1
+  return (vim.fn.filereadable(expanded) == 1) or (vim.fn.isdirectory(expanded) == 1)
 end
 
 return M
