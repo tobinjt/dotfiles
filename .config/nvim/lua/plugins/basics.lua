47c7b705e12d22ca8f48ac76879119f6174613b1
John Tobin <johntobin@johntobin.ie>
2025-07-05 21:58:32 +0100

    fix(style): sort list

--- .config/nvim/lua/plugins/basics.lua
+++ .config/nvim/lua/plugins/basics.lua
@@ -1,12 +1,5 @@
 return {
-  {
-    -- Improved status bar.
-    "vim-airline/vim-airline",
-  },
-  {
-    -- Makes filename:line-number[:column] work.
-    "wsdjeg/vim-fetch",
-  },
+  -- keep-sorted start block=true by_regex=".*"
   {
     -- Jumps to the last exited location in files.
     "farmergreg/vim-lastplace",
@@ -15,4 +8,13 @@ return {
     -- VCS indicators in the signs column.
     "mhinz/vim-signify",
   },
+  {
+    -- Improved status bar.
+    "vim-airline/vim-airline",
+  },
+  {
+    -- Makes filename:line-number[:column] work.
+    "wsdjeg/vim-fetch",
+  },
+  -- keep-sorted end
 }
