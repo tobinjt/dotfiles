8c04fee309360fe1fed5701f5ebfea8df5aa0702
John Tobin <johntobin@johntobin.ie>
2025-07-10 21:36:37 +0100

    feat: implement switching between Python source and test files.

--- .config/nvim/lua/plugins/other.lua
+++ .config/nvim/lua/plugins/other.lua
@@ -10,15 +10,42 @@ return {
     keys = {
       {
         "<leader>rf",
-        "<cmd>OtherVSplit",
+        "<cmd>OtherVSplit<cr>",
         desc = "Open other.nvim for vertical split",
       },
     },
     opts = {
       mappings = {
         "golang",
-        "python",
         "rust",
+        {
+          context = "go to source file",
+          pattern = "(.*)_test.py$",
+          target = "%1.py",
+        },
+        {
+          context = "go to test file",
+          pattern = function(filename)
+            -- If filename is a test file, return nil so this rule is skipped.
+            local test_match = vim.fn.matchlist(filename, "\\v.*_test.py$")
+            if #test_match > 0 then
+              return nil
+            end
+
+            -- Is this a non-test Python file?
+            local match = vim.fn.matchlist(filename, "\\v(.*).py$")
+            if #match > 0 then
+              -- The first element is the full matching string, and subsequent
+              -- elements are the captured groups, so remove the first element.
+              table.remove(match, 1)
+              return match
+            end
+
+            -- No match.
+            return nil
+          end,
+          target = "%1_test.py",
+        },
       },
     },
   },
