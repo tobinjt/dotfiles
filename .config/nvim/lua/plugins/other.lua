2baa5445a058b4d15fea589076579220ff23c581
John Tobin <johntobin@johntobin.ie>
2025-08-17 21:12:13 +0100

    feat: generalise support for not matching test files.

--- .config/nvim/lua/plugins/other.lua
+++ .config/nvim/lua/plugins/other.lua
@@ -1,3 +1,32 @@
+--- Returns the filename without the suffix if it is not a test file.
+--
+-- This function is used to implement custom pattern matchers for the other.nvim
+-- plugin. It ensures that the rule only applies to non-test files.
+--
+-- @param filename (string) The full filename to check.
+-- @param suffix (string) The file suffix (e.g., "py", "cc") to look for.
+-- @return (table|nil) A table containing the filename without the suffix if it's
+--   a non-test file, otherwise nil. The table is what other.nvim expects.
+local dont_match_test_files = function(filename, suffix)
+  -- If filename is a test file, return nil so this rule is skipped.
+  local test_match = vim.fn.matchlist(filename, "\\v.*_test." .. suffix .. "$")
+  if #test_match > 0 then
+    return nil
+  end
+
+  -- Is this a non-test file?
+  local match = vim.fn.matchlist(filename, "\\v(.*)." .. suffix .. "$")
+  if #match > 0 then
+    -- The first element is the full matching string, and subsequent
+    -- elements are the captured groups, so remove the first element.
+    table.remove(match, 1)
+    return match
+  end
+
+  -- No match.
+  return nil
+end
+
 return {
   {
     "rgroli/other.nvim",
@@ -18,7 +47,6 @@ return {
       mappings = {
         "golang",
         "rust",
-        -- TODO: generalise this, maybe with a transformer?
         {
           context = "Python go to source file",
           pattern = "(.*)_test.py$",
@@ -27,23 +55,7 @@ return {
         {
           context = "Python go to test file",
           pattern = function(filename)
-            -- If filename is a test file, return nil so this rule is skipped.
-            local test_match = vim.fn.matchlist(filename, "\\v.*_test.py$")
-            if #test_match > 0 then
-              return nil
-            end
-
-            -- Is this a non-test Python file?
-            local match = vim.fn.matchlist(filename, "\\v(.*).py$")
-            if #match > 0 then
-              -- The first element is the full matching string, and subsequent
-              -- elements are the captured groups, so remove the first element.
-              table.remove(match, 1)
-              return match
-            end
-
-            -- No match.
-            return nil
+            return dont_match_test_files(filename, "py")
           end,
           target = "%1_test.py",
         },
@@ -55,23 +67,7 @@ return {
         {
           context = "CPP go to test file",
           pattern = function(filename)
-            -- If filename is a test file, return nil so this rule is skipped.
-            local test_match = vim.fn.matchlist(filename, "\\v.*_test.cc$")
-            if #test_match > 0 then
-              return nil
-            end
-
-            -- Is this a non-test CPP file?
-            local match = vim.fn.matchlist(filename, "\\v(.*).cc$")
-            if #match > 0 then
-              -- The first element is the full matching string, and subsequent
-              -- elements are the captured groups, so remove the first element.
-              table.remove(match, 1)
-              return match
-            end
-
-            -- No match.
-            return nil
+            return dont_match_test_files(filename, "cc")
           end,
           target = "%1_test.cc",
         },
