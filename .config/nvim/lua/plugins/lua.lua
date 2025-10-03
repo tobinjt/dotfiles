6a98cccafe4607707dab7e59aae659389fa0f3db
John Tobin <johntobin@johntobin.ie>
2025-10-03 20:31:50 +0100

    feat: configure root_markers so that Exercism exercises will be self-contained.

--- .config/nvim/lua/plugins/lua.lua
+++ .config/nvim/lua/plugins/lua.lua
@@ -30,6 +30,29 @@ return {
     opts = {
       enabled_servers = {
         lua_ls = {
+          root_markers = {
+            -- This is copied from nvim-lspconfig/lsp/lua_ls.lua and extended
+            -- with .exercism so that when working on Exercism exercises I only
+            -- load the current exercise not the entire set.
+            -- I would prefer to load the base list from nvim-lspconfig, but
+            -- that means either I require that module which will break
+            -- bootstrapping, or I make this opts a function and multiple opts
+            -- functions can be tricky to manage.
+            -- TODO: experiment with making this a function.
+            --
+            -- keep-sorted start
+            ".exercism",
+            ".luacheckrc",
+            ".luarc.json",
+            ".luarc.jsonc",
+            ".stylua.toml",
+            "selene.toml",
+            "selene.yml",
+            "stylua.toml",
+            -- keep-sorted end
+            -- Keep .git to the end because the others are more specific.
+            ".git",
+          },
           settings = {
             Lua = {
               diagnostics = {
