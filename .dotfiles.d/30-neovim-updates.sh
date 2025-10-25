dfca39a6e74090f41c057521d40f3d4c719ca4b7
John Tobin <johntobin@johntobin.ie>
2025-10-25 22:03:24 +0100

    feat: install treesitter parsers.

--- .dotfiles.d/30-neovim-updates.sh
+++ .dotfiles.d/30-neovim-updates.sh
@@ -5,16 +5,16 @@ set -e -f -u -o pipefail
 main() {
   # Clean out treesitter .so files, otherwise they are never updated.
   #
-  # I used to run :TSUpdate here, but it fails sometimes, so I stopped.  That
-  # means that they will be compiled the next time I run nvim interactively, but
-  # that's acceptable if not perfect.
-  #
   # Note to self: this should be the last action so that it doesn't cause
   # problems for other nvim invocations.
   local treesitter_dir="${HOME}/.local/share/nvim/lazy/nvim-treesitter/parser"
   if [[ -d "${treesitter_dir}" ]]; then
     find "${treesitter_dir}" -type f -mtime +1 -name '*.so' -delete
   fi
+  # TSUpdateSync doesn't block nvim exiting, so sleep for 10 seconds to give it
+  # time to download and install.  10 seconds worked in testing and didn't feel
+  # too long.
+  run-nvim-command ":TSUpdateSync" -c "sleep 10"
 }
 
 main "$@"
