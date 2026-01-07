6586daa4ee8463ac4d69012314b3fc395a7872c6
John Tobin <johntobin@johntobin.ie>
2026-01-07 13:53:51 +0000

    fix(refactor): make ToggleInlayHints into a function for reuse.

--- .config/nvim/lua/johntobin/commands.lua
+++ .config/nvim/lua/johntobin/commands.lua
@@ -1,27 +1,4 @@
-local toggle_inlay_hints = function()
-  local bufnr = vim.api.nvim_get_current_buf()
+local functions = require('johntobin.functions')
 
-  -- Check if any attached LSP client supports inlay hints
-  local clients = vim.lsp.get_clients({ bufnr = bufnr })
-  local supports_inlay = false
-  for _, client in ipairs(clients) do
-    if client:supports_method("textDocument/signatureHelp") then
-      supports_inlay = true
-      break
-    end
-  end
-  if not supports_inlay then
-    vim.notify("No LSP client attached to this buffer supports inlay hints.",
-      vim.log.levels.WARN)
-    return
-  end
-
-  -- Toggle inlay hints for the current buffer
-  local is_enabled = vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr })
-  vim.lsp.inlay_hint.enable(not is_enabled, { bufnr = bufnr })
-  vim.notify("Inlay hints " .. (is_enabled and "disabled" or "enabled"),
-    vim.log.levels.INFO)
-end
-
-vim.api.nvim_create_user_command('ToggleInlayHints', toggle_inlay_hints,
+vim.api.nvim_create_user_command('ToggleInlayHints', functions.ToggleInlayHints,
   { desc = "Toggle LSP inlay hints for the current buffer" })
--- .config/nvim/lua/johntobin/functions.lua
+++ .config/nvim/lua/johntobin/functions.lua
@@ -82,4 +82,29 @@ M.RunCommandIfExists = function(command_name, ...)
   end
 end
 
+M.ToggleInlayHints = function()
+  local bufnr = vim.api.nvim_get_current_buf()
+
+  -- Check if any attached LSP client supports inlay hints
+  local clients = vim.lsp.get_clients({ bufnr = bufnr })
+  local supports_inlay = false
+  for _, client in ipairs(clients) do
+    if client:supports_method("textDocument/signatureHelp") then
+      supports_inlay = true
+      break
+    end
+  end
+  if not supports_inlay then
+    vim.notify("No LSP client attached to this buffer supports inlay hints.",
+      vim.log.levels.WARN)
+    return
+  end
+
+  -- Toggle inlay hints for the current buffer
+  local is_enabled = vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr })
+  vim.lsp.inlay_hint.enable(not is_enabled, { bufnr = bufnr })
+  vim.notify("Inlay hints " .. (is_enabled and "disabled" or "enabled"),
+    vim.log.levels.INFO)
+end
+
 return M
