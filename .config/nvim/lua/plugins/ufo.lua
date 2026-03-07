a2cb5c376342690bd7f4b58ff2c3f48bfaba72bd
John Tobin <johntobin@johntobin.ie>
2026-03-07 18:33:23 +0000

    feat: set up UFO for folding

---
+++ .config/nvim/lua/plugins/ufo.lua
@@ -0,0 +1,43 @@
+-- Fold options are required for ufo to work
+vim.o.foldcolumn = '1' -- This is optional.
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+
+return {
+  'kevinhwang91/nvim-ufo',
+  dependencies = {
+    'kevinhwang91/promise-async',
+  },
+  lazy = false,
+
+  keys = {
+    {
+      'zR',
+      function() require('ufo').openAllFolds() end,
+      desc = 'Open all folds',
+    },
+    {
+      'zM',
+      function() require('ufo').closeAllFolds() end,
+      desc = 'Close all folds',
+    },
+    {
+      'zP',
+      function()
+        local peeked = require('ufo').peekFoldedLinesUnderCursor()
+        if not peeked then
+          vim.lsp.buf.hover()
+        end
+      end,
+      desc = 'Peek Fold',
+    },
+  },
+
+  opts = {
+    provider_selector = function(_, _, _)
+      return { 'lsp', 'indent' }
+      -- return { 'treesitter', 'indent' }
+    end
+  }
+}
